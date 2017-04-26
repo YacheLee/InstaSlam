4a5af4447b7265a1796a5a099bf8c88d02fbd305
Scott <mymislife@gmail.com>
2017-04-26 11:03:19 +0800

    set image button of fab

--- src/main/java/com/dentaltw/instaslam/activities/MainActivity.java
+++ src/main/java/com/dentaltw/instaslam/activities/MainActivity.java
@@ -1,23 +1,19 @@
 package com.dentaltw.instaslam.activities;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 import com.dentaltw.instaslam.R;
 import com.dentaltw.instaslam.fragments.FeedActivityFragment;
 import com.dentaltw.instaslam.fragments.HomeFragment;
@@ -60,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(mViewPager);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setImageDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.new_post_btn));
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
